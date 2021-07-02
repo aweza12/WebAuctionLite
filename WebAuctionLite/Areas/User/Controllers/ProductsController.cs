2b35335fd1c21522039f71d5c69d6dd51e5ead21
Марчук Евгений <aweza12@gmail.com>
2021-07-02 17:49:19 +0300

    2

--- WebAuctionLite/Areas/User/Controllers/ProductsController.cs
+++ WebAuctionLite/Areas/User/Controllers/ProductsController.cs
@@ -13,15 +13,49 @@ namespace WebAuctionLite.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostingEnvironment;
 
-        public ProductsController()
+        public ProductsController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
         {
             this.dataManager = dataManager;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         public IActionResult Index()
         {
-            return View(dataManager.ServiceItems.GetServiceItems());
+            return View(dataManager.Products.GetProducts());
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            var entity = id == default ? new Product() : dataManager.Products.GetProductById(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product model, IFormFile titleImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (titleImageFile != null)
+                {
+                    model.TitleImagePath = titleImageFile.FileName;
+                    using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, "images/", titleImageFile.FileName), FileMode.Create))
+                    {
+                        titleImageFile.CopyTo(stream);
+                    }
+                }
+                dataManager.Products.SaveProduct(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Guid id)
+        {
+            dataManager.Products.DeleteProduct(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
         }
     }
 }
