0fb82f4c0b4c988bf23dd610fcade1da1f1e9e0e
Марчук Евгений <aweza12@gmail.com>
2021-07-05 15:14:47 +0300

    0502

--- WebAuctionLite/Areas/User/Controllers/ApplicationUserController.cs
+++ WebAuctionLite/Areas/User/Controllers/ApplicationUserController.cs
@@ -37,7 +37,6 @@ namespace WebAuctionLite.Areas.User.Controllers
             {
                 return NotFound();
             }
-            //ApplicationUserViewModel model = new ApplicationUserViewModel { FirstName = user.FirstName, LastName = user.LastName, UserName = user.UserName, Email = user.Email, };
             ApplicationUser model = user;
             return View(model);
         }
@@ -45,21 +44,6 @@ namespace WebAuctionLite.Areas.User.Controllers
         [HttpPost]
         public async Task<IActionResult> Edit(ApplicationUser model)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    ApplicationUser user = (ApplicationUser)await userManager.FindByNameAsync(model.UserName);
-            //    if (user != null)
-            //    {
-            //        user.Email = model.Email;
-            //        user.UserName = model.Email;
-            //        user.FirstName = model.FirstName;
-            //        user.LastName = model.LastName;
-            //        user.UserName = model.UserName;
-
-            //        dataManager.ApplicationUsers.SaveApplicationUser(user);
-            //        return RedirectToAction("Index");
-            //    }
-            //}
             if (ModelState.IsValid)
             {
                 ApplicationUser user = (ApplicationUser)userManager.FindByIdAsync(userManager.GetUserId(User)).Result;
@@ -71,12 +55,59 @@ namespace WebAuctionLite.Areas.User.Controllers
                     user.LastName = model.LastName;
                     user.UserName = model.UserName;
 
-                    //dataManager.ApplicationUsers. .SaveApplicationUser(user);
                     await userManager.UpdateAsync(user);
                     return RedirectToAction("Index");
                 }
             }
             return View();
+        }
+
+        public async Task<IActionResult> ChangePassword()
+        {
+            IdentityUser user = userManager.FindByIdAsync(userManager.GetUserId(User)).Result;
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ChangePasswordViewModel model = new ChangePasswordViewModel { Id = user.Id, Email = user.Email };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = userManager.FindByIdAsync(userManager.GetUserId(User)).Result;
+                if (user != null)
+                {
+                    var _passwordValidator =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordValidator<IdentityUser>)) as IPasswordValidator<IdentityUser>;
+                    var _passwordHasher =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordHasher<IdentityUser>)) as IPasswordHasher<IdentityUser>;
+
+                    IdentityResult result =
+                        await _passwordValidator.ValidateAsync(userManager, user, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        user.PasswordHash = _passwordHasher.HashPassword(user, model.NewPassword);
+                        await userManager.UpdateAsync(user);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+            }
+            return View(model);
         }
 
         [HttpPost]
---
+++ WebAuctionLite/Areas/User/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAuctionLite.Areas.User.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public string Id { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+    }
+}
